857bb69adba7b0399a6bbfd267935b2cd79a1a60
rzryr1 <solotogre@gmail.com>
2024-12-17 16:38:50 +0700

    lab10_2.cpp completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -8,7 +8,12 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
+	dest << "-------------------- BOOM ---------------------\n";		
+	string textline;	
+	while(getline (source,textline)){		
+		dest << textline << endl;
+	}
+	dest << "-------------------- HA!! ---------------------";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
